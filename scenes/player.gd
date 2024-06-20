845569895e6d705648737e2ff737e25cacc156b1
NewtKingGames <newtkinggames@gmail.com>
2024-06-19 21:14:39 -0500

    Add sprinting

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-
 var is_aiming: bool = false
 var is_shooting: bool = false
+var is_sprinting: bool = false 
 
 var player_aim_walk_speed: int = 100
-var player_sprint_speed: int = 600
+var player_sprint_speed: int = 400
 var player_walk_speed: int = 300
+
 
 var aim_reticle = load("res://crosshair007.png")
 var mouse_reticle = load("res://crosshair001.png")
@@ -16,19 +17,21 @@ func _process(_delta):
 		Input.set_custom_mouse_cursor(aim_reticle)
 	else:
 		Input.set_custom_mouse_cursor(mouse_reticle)
-	# Temporary while we mess with shadows!!!
 	look_at(get_global_mouse_position())
-	# TODO Medium refactor of these nasty booleans
 	# TODO look up animated trees and state machines to see how you can handle this nicer
-	# Eventually cleanup the sprite images themselves to put the player in a consistent posistion
+	# TODO cleanup the sprite images themselves to put the player in a consistent posistion
+
 	# Grab the direction first so we can use it in the else block and the movement section
 	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction and not is_aiming and Input.is_action_pressed("sprint"): 
+		is_sprinting = true
+	else:
+		is_sprinting = false
 	if Input.is_action_pressed("aim"):
 		look_at(get_global_mouse_position())
 		if not is_aiming:
 			$CharacterAnimatedSprite2D.play("aim")
 		is_aiming = true
-		# TODO could have the player "frozen" until they have finished aiming and then they can move
 		if Input.is_action_just_pressed("shoot"):
 			print("Shoot")
 			is_shooting = true
@@ -36,20 +39,28 @@ func _process(_delta):
 			$Gun.shoot(aim_direction)
 	else:
 		# TODO could play a nice reverse aim animation
-		# If we are not aiming face the player in the direction we are moving
 		is_aiming = false
-		if direction != Vector2.ZERO:
-			look_at(direction + position)
+		if direction != Vector2.ZERO and is_sprinting:
+			look_at(direction + position) # Only look at direction when sprinting?
 	
 	if is_shooting:
 		return
+	
 	# Calculate player velocity
-	var current_speed = player_aim_walk_speed if is_aiming else player_walk_speed
+	var current_speed = 0
+	if is_aiming:
+		current_speed = player_aim_walk_speed
+	elif is_sprinting:
+		current_speed = player_sprint_speed
+	else:
+		current_speed = player_walk_speed
 	velocity = direction * current_speed
 	if direction != Vector2.ZERO:
 		if is_aiming:
 			# TODO reconcile this with shooting, right now you can aim and shoot and the animation won't play
 			$CharacterAnimatedSprite2D.play("aim_walk")
+		elif is_sprinting:
+			$CharacterAnimatedSprite2D.play("sprint")
 		else:
 			# Play walking animation
 			$CharacterAnimatedSprite2D.play("walk")
